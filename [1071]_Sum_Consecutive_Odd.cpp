3e1209e803feafad619c6b0ede7259edd0b664cb
Ashik Rahman <81816852+ashik5757@users.noreply.github.com>
2021-05-11 03:23:34 +0600

    URI

---
+++ [1071]_Sum_Consecutive_Odd.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+
+    int x,y,temp,sum=0;
+
+    cin >> x >> y;
+
+    if (x>y) {
+
+        temp=x;
+        x=y;
+        y=temp;
+    }
+
+    for (int i=x+1;i<y;i++) {
+
+        if (i%2!=0) {
+            sum+=i;
+        }
+
+    }
+
+    cout << sum << endl;
+ 
+    system("pause");
+    
+    return 0;
+}
