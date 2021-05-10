3e286f616dcc5b8b605690994558784370cc9427
Ashik Rahman <81816852+ashik5757@users.noreply.github.com>
2021-05-11 02:46:00 +0600

    URI

---
+++ [1047]_altr_Game_time_minutes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+
+    int h1,m1,h2,m2;
+
+    cin >> h1 >> m1 >> h2 >> m2;
+
+    int h = h2-h1;
+    int m = m2-m1;
+
+    if (m<0) {
+
+        m+=60;
+        h--;
+
+    }
+
+    if (h<0) {
+
+        h+=24;
+    }
+
+     cout << "O JOGO DUROU " << h << " HORA(S)" << " E " << m << " MINUTO(S)" << endl;
+           
+
+
+    system("pause");
+    
+    return 0;
+}
---
+++ [1061]_Event_time.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+
+    string trash;
+
+    int d1,d2;
+    int h1,h2,m1,m2,s1,s2;
+
+    cin >> trash >> d1;
+    cin >> h1 >> trash >> m1 >> trash >> s1;
+    cin >> trash >> d2;
+    cin >> h2 >> trash >> m2 >> trash >> s2;
+
+    int s = s2 - s1;
+    int m = m2 - m1;
+    int h = h2 - h1;
+    int d = d2 - d1;
+
+    if(s<0){
+
+	    s+=60;
+	    m--;
+    }
+
+    if(m<0){
+
+	    m+=60;
+	    h--;
+    }
+
+    if(h<0){
+        
+	    h+=24;
+	    d--;
+    }
+
+   
+
+  cout << d << " dia(s)" << endl;
+  cout << h << " hora(s)" << endl;
+  cout << m << " minuto(s)" << endl;
+  cout << s << " segundo(s)" << endl;
+  
+
+
+
+    system("pause");
+    
+    return 0;
+}
