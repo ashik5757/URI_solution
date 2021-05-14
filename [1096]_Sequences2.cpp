f84333215bc8f2205621ed8be36ba755d006d3b2
Ashik Rahman <81816852+ashik5757@users.noreply.github.com>
2021-05-14 12:38:27 +0600

    C++

---
+++ [1096]_Sequences2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+
+    int i,j;
+
+   
+
+        for (i=1;i<=9;i+=2) {
+
+            j=7;
+
+           for (int a=1;a<=3;a++) {
+
+            cout << "I=" << i << " J=" << j << endl;
+
+            j--;
+
+           } 
+            
+             
+        }
+
+
+            
+
+
+
+    
+    system("pause");
+    
+    return 0;
+}
